f33ecbdb5656e9b758de4f334e7bab0f6afc9c77
Kim JunHyeong <kjh20000225@naver.com>
2022-10-05 00:16:00 +0900

    Auth test

--- db_manager/serializers.py
+++ db_manager/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from db_manager.models import ECG_data
+from django.contrib.auth.models import User
 
 class ECG_dataSerializer(serializers.ModelSerializer):
     class Meta:
         model = ECG_data
         fields = ('ecg', 'id')
+        
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'id')
--- db_manager/views.py
+++ db_manager/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from db_manager.models import ECG_data
 from db_manager.serializers import ECG_dataSerializer
+from db_manager.serializers import UserSerializer
+from django.contrib.auth.models import User
 # Create your views here.
 
 class ECG_dataViewSet(viewsets.ModelViewSet):
     queryset = ECG_data.objects.all()
+    queryset2 = User.objects.all()
     serializer_class = ECG_dataSerializer
+    serializer_class2 = UserSerializer
