941c02316f0813732b705ea642630f29f9b5b860
JudeKim <kjh20000225@naver.com>
2022-10-05 17:22:03 +0900

    admin site add user

--- db_manager/admin.py
+++ db_manager/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from ECG_test2.db_manager.models import User_data
 from db_manager.models import ECG_data
+from db_manager.models import User_data
 
 admin.site.register(ECG_data)
+admin.site.register(User_data)
 # Register your models here.
