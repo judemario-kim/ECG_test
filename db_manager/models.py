abd51fdc37fbbfe474741f8ca7b4e87c0b1f012a
JudeKim <kjh20000225@naver.com>
2022-10-05 20:47:47 +0900

    ecg model edit

--- db_manager/models.py
+++ db_manager/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class ECG_data(models.Model):
     ecg = models.CharField(max_length = 1024)
-    ecg_user = models.EmailField()
+    ecg_user = models.EmailField(default = "")
 
     def __str__(self):
         return self.ecg
