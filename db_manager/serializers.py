cb9e5f7800f090f8314c363b8a923242aa2f6e84
JudeKim <kjh20000225@naver.com>
2022-11-25 11:02:32 +0900

    datetime field

--- db_manager/serializers.py
+++ db_manager/serializers.py
@@ -5,7 +5,7 @@ from db_manager.models import User_data
 class ECG_dataSerializer(serializers.ModelSerializer):
     class Meta:
         model = ECG_data
-        fields = ('ecg', 'ecg_user', 'id')
+        fields = ('ecg', 'ecg_user', 'created_date', 'id')
         
 class User_dataSerializer(serializers.ModelSerializer):
     class Meta:
